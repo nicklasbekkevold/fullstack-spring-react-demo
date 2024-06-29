61941a71c3948d85432ee50739d9a2e146076bdd
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2024-06-29 12:43:22 +0200

    Add smoke tests for context path

---
+++ src/test/java/com/example/springboot/SmokeTest.java
@@ -0,0 +1,19 @@
+package com.example.springboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+
+    @Autowired
+    private HelloController helloController;
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(helloController).isNotNull();
+    }
+}
